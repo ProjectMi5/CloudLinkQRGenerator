24c7047b8d45e4c9eff1ec2b3fbba7e99dfda0db
frth <fruth@itq.de>
2015-11-17 17:27:18 +0100

    update config.sample.js

--- config.sample.js
+++ config.sample.js
@@ -4,10 +4,14 @@
 var config = {};
 
 config.rest = {};
-config.rest.host = 'https://foo.bar.xz';
-config.rest.getTasks = 'getTasks';
+config.rest.host = 'https://foo.bar.xy';
+config.rest.validateVoucher = 'validateVoucher';
 config.auth = {}; // used with request - keep object properties user and password!
 config.auth.user = 'foo';
 config.auth.password = 'bar';
+
+config.qr = {};
+config.qr.url = "http://foo.bar.xy/qr";
+config.qr.target = "./QR_Codes/"; // relative to the file that calls generator.js
 
 module.exports = config;

6a6ea7fa91a1b4f2b8718c8dba1af5ed3d5ea6c5
Thomas Frei <mail@thomasfrei.de>
2015-11-18 15:04:06 +0100

    optimize config for local testing

--- config.sample.js
+++ config.sample.js
@@ -1,17 +1,33 @@
 /**
- * Sample Configuration
+ * Configuration
  */
 var config = {};
 
-config.rest = {};
-config.rest.host = 'https://foo.bar.xy';
-config.rest.validateVoucher = 'validateVoucher';
+// Secure Config
 config.auth = {}; // used with request - keep object properties user and password!
 config.auth.user = 'foo';
 config.auth.password = 'bar';
 
+
+config.http = {};
+
+config.rest = {};
+config.rest.host = 'https://mi5.itq.de';
+config.rest.getTasks = 'getTasks';
+config.rest.getOrdersByStatus = 'getOrdersByStatus';
+config.rest.updateOrderStatus = 'updateOrderStatus';
+config.rest.updateOrder = 'updateOrder';
+
 config.qr = {};
 config.qr.url = "http://foo.bar.xy/qr";
 config.qr.target = "./QR_Codes/"; // relative to the file that calls generator.js
+
+// Run on localhost
+if(process.env.TEST){
+  config.rest.host = 'http://localhost:3001';
+  config.auth.user = 'foo';
+  config.auth.password = 'bar';
+  config.rest.placeOrder = 'placeOrder';
+}
 
 module.exports = config;
